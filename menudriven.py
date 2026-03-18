1eb8395ad07afea28c9fb3dc881f8e54337181f3
salena188 <salinashrestha188@gmail.com>
2026-03-18 20:04:21 +0545

    changes in menudriven

--- menudriven.py
+++ menudriven.py
@@ -12,19 +12,19 @@ while True:
     choice = int(input("Enter your choice (1-6): "))
 
     if choice == 1:
-        print("BMI Calculator: https://github.com/bidhyabhattarai/BMI_calculator")
+        print("BMI Calculator link: https://github.com/bidhyabhattarai/BMI_calculator")
 
     elif choice == 2:
-        print("Taxation System: https://github.com/Anujakhatri/Taxation-system-nepal")
+        print("Taxation System link: https://github.com/Anujakhatri/Taxation-system-nepal")
 
     elif choice == 3:
-        print("Currency COnverter Link: https://github.com/sneharitas/currency_converter")
+        print("Currency COnverter link: https://github.com/sneharitas/currency_converter")
 
     elif choice == 4:
-        print("Remittance Link: https://github.com/binisha77/nepal_remittance_calculator")
+        print("Remittance link: https://github.com/binisha77/nepal_remittance_calculator")
 
     elif choice == 5:
-        print("Lagani Kosh Link: https://github.com/kopiladevkota/nepal_lagani_kosh")
+        print("Lagani Kosh link: https://github.com/kopiladevkota/nepal_lagani_kosh")
 
     elif choice == 6:
         print("Exiting program...")
