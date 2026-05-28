c93a9c6550858d211f72f72a56e116ac77fd1e1a
salena188 <salinashrestha188@gmail.com>
2026-05-28 14:30:11 +0545

    user input example for string

--- string.py
+++ string.py
@@ -1,8 +1,16 @@
-#sequence of characters
+#strings
 name = "salina"
 city = "bhaktapur"
+
+#acessing the character
 print(name[0])
 print(name.upper())
 print(name.lower())
 print(name.capitalize())
 print(name.title()) # len()
+
+#user input
+user_name = input("Enter your name: ")
+print("Upper:" ,user_name.upper())
+print("Lower:" ,user_name.lower())
+print("length:", len(user_name))
