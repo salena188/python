4a334cf5e588297326c55eff7e21f677489ae78f
salena188 <salinashrestha188@gmail.com>
2026-05-28 12:59:34 +0545

    quew update

--- advance-datastructure.py
+++ advance-datastructure.py
@@ -11,3 +11,37 @@ stack.pop()  # removing the top element of the stack
 print(stack)
 print(stack.pop())
 
+#queue
+from collections import deque
+queue = deque()
+
+queue.append("Customer1")
+queue.append("Customer2")
+queue.append("Customer3")
+
+print("Queue:", queue)
+
+served = queue.popleft()
+
+print("Served:", served)
+print("Remaining:", queue)
+
+
+#queue practise
+from collections import deque
+
+orders = deque()
+
+orders.append("Pizza")
+orders.append("Burger")
+orders.append("Momo")
+
+print("Orders:", orders)
+
+served = orders.popleft()
+
+print("Served:", served)
+
+print("Remaining Orders:", orders)
+
+
